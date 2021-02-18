883e9e364f95e14255f757d0187f39d21f2cb3b8
alexey.mitsevich <alexey.mitsevich@vrpconsulting.com>
2021-02-18 20:24:35 +0300

    add logic for addind sync and async post

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { rootReducer } from "./redux/rootReducer";
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+)
+
+render( app, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
